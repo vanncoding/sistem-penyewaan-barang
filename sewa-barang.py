0cd6492d0bdba78908068850162b064db286d4a0
Revan <revandra033@gmail.com>
2025-12-09 13:47:51 +0700

    Membuat Tampilan Awal

--- sewa-barang.py
+++ sewa-barang.py
@@ -1,5 +1,24 @@
-print ("TESSSSTTTT")
+kendaraan = []
 
-print ("TEST 2")
+print ("1. Sewa Kendaraan")
+print ("2. List Kendaraan")
+print ("4. Exit")
+print ("=" * 20)
+pilih_menu = int(input("Masukan Menu :"))
 
-print ("TESTTT 3")
+if pilih_menu == 1:
+    print("1. Totoyota")
+    print("2. BMW")
+    print("3. Daihatsu")
+    kode = int(input("Masukan Kode Kendaraan: "))
+    if kode == 1:
+        print("1. Avanza")
+        print("2. Inova")
+        print("3. Calya")
+        print("4. Agya")
+        print("=" * 20)
+        kode_toyota = int(input("Masukan No Sewa :"))
+    else :
+        print("KODE YANG ANDA MASUKAN SALAH")
+else:
+    print("Masukan Kode :")
